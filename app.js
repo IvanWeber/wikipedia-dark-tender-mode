efb8276eef90e161033d2d325702eebeecbb6154
IvanWeber <ivanbdn@yandex.ru>
2021-09-01 00:51:51 +0300

    initial

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("wikipedia dark mode plugin enabled")
+
+//let content = document.getElementById("content")
+
+//content.setAttribute("style", "color: e6e6e6 !important; background-color: #333333;")
